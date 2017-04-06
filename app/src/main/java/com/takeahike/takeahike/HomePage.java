74dc590f1e5cee73cc736be2a52154786e585d90
Wesley Olinger <Wesley Olinger>
2017-04-05 23:31:34 -0400

    Second push

--- app/src/main/java/com/takeahike/takeahike/HomePage.java
+++ app/src/main/java/com/takeahike/takeahike/HomePage.java
@@ -3,15 +3,22 @@ package com.takeahike.takeahike;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ViewFlipper;
 
 /**
  * Created by wesle on 3/19/2017.
  */
 
-public class HomePage extends Fragment{
+public class HomePage extends Fragment {
+
+    Animation Fade_in, Fade_out;
+    ViewFlipper viewFlipper;
 
     @Nullable
     @Override
@@ -22,6 +29,6 @@ public class HomePage extends Fragment{
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.pageinfo, container, false);
+        return inflater.inflate(R.layout.home, container, false);
     }
 }
