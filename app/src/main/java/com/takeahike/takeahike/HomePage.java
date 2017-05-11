ae6cd9e13f6cd60c1c5255fe21ecf3e261b128af
Brady Winter <bw002@eagles.bridgewater.edu>
2017-05-10 20:51:24 -0400

    got rid of settings

--- app/src/main/java/com/takeahike/takeahike/HomePage.java
+++ app/src/main/java/com/takeahike/takeahike/HomePage.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 /**
@@ -24,9 +27,12 @@ public class HomePage extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         getActivity().setTitle("Home Page");
+
+
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.home, container, false);
     }
 }
